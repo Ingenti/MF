0321a1359c74c09f2e224d3bdf43555ad85ad7be
Akseli <akseli.oinaanoja@aalto.fi>
2021-05-05 12:02:49 +0300

    2

--- mf2/mf.cc
+++ mf2/mf.cc
@@ -13,16 +13,16 @@ This is the function you need to implement. Quick reference:
 */
 void mf(int ny, int nx, int hy, int hx, const float *in, float *out) 
 {
-  std::vector<double> v;
+  #pragma omp parallel for
   for(int y = 0; y < ny; y++)
   {
+    std::vector<double> v;
     for(int x = 0; x < nx; x++)
     {
       int alkub = std::max(y-hy,0);
       int alkua = std::max(x-hx,0);
       int loppub = std::min(y+hy+1,ny);
       int loppua = std::min(x+hx+1,nx);
-      #pragma omp parallel for
       for(int b = alkub; b < loppub; b++)
       {
         for(int a = alkua; a < loppua; a++)
@@ -31,17 +31,15 @@ void mf(int ny, int nx, int hy, int hx, const float *in, float *out)
         }
         
       }
+      std::nth_element(v.begin(), v.begin() + v.size()/2, v.end());
       if(v.size() % 2 == 1)
       {
-        std::nth_element(v.begin(), v.begin() + v.size()/2, v.end());
         out[x+y*nx] = v[v.size()/2];
       }
       else
       {
-        std::nth_element(v.begin(), v.begin() + v.size()/2, v.end());
         out[x+y*nx] = (v[v.size()/2] + v[v.size()/2-1]) / 2;
       }
-      v.clear();
     }
   }
 }
