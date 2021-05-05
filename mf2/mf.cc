f0eef67bda48458694694bd0901ad746609cfc92
Akseli <akseli.oinaanoja@aalto.fi>
2021-05-05 12:11:05 +0300

    2

--- mf2/mf.cc
+++ mf2/mf.cc
@@ -19,6 +19,7 @@ void mf(int ny, int nx, int hy, int hx, const float *in, float *out)
   {
     for(int x = 0; x < nx; x++)
     {
+      v = {};
       int alkub = std::max(y-hy,0);
       int alkua = std::max(x-hx,0);
       int loppub = std::min(y+hy+1,ny);
