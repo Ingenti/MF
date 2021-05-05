b4f561d2e23763e940f3d7d1f7ea197197d42a81
Akseli <akseli.oinaanoja@aalto.fi>
2021-05-05 12:00:52 +0300

    2

--- mf2/mf.cc
+++ mf2/mf.cc
@@ -28,6 +28,7 @@ void mf(int ny, int nx, int hy, int hx, const float *in, float *out)
         for(int a = alkua; a < loppua; a++)
         {
           v.push_back(in[a+b*nx]);
+          v.
         }
         
       }
@@ -39,7 +40,6 @@ void mf(int ny, int nx, int hy, int hx, const float *in, float *out)
       else
       {
         std::nth_element(v.begin(), v.begin() + v.size()/2, v.end());
-        std::nth_element(v.begin(), v.begin() + (v.size()/2)-1, v.end());
         out[x+y*nx] = (v[v.size()/2] + v[v.size()/2-1]) / 2;
       }
       v.clear();
